850769a56e11633f71be36e3920958baa19caa09
JesusVaPa <vapa.jesusangel@gmail.com>
2024-09-09 11:55:10 -0400

    Education page fixed

--- src/components/education.js
+++ src/components/education.js
@@ -8,11 +8,11 @@ function Education() {
     <div id='education'>
       <p className='sectionTitle'>EDUCATION</p>
       <div className='stdBox'>               
-        <h1 >IT Programmer-Analyst ASC<span className='date'>May 2023 - December 2024</span></h1>
+        <h1 className='programName'>IT Programmer-Analyst ASC<span className='date'>May 2023 - December 2024</span></h1>
         <a href='https://lasallecollege.lcieducation.com/' target="_blank" rel='noreferrer' className='emphText' ><span><img src={LSClogo} alt="LSClogo" className="instlogo-img" /></span>LaSalle College, Montreal, QC.</a>
       </div>
       <div className='stdBox'>        
-        <h1>Electromechanics Engineering<span className='date'>August 2016 - June 2021</span></h1>
+        <h1 className='programName'>Electromechanics Engineering<span className='date'>August 2016 - June 2021</span></h1>
         <a href='https://www.tecnm.mx/' target="_blank" rel='noreferrer' className='emphText'><span><img src={TNMlogo} alt="TNMlogo" className="instlogo-img" /></span>Instituto Tecnologico Nacional De Mexico. Leon, MX.</a>
       </div>                    
     </div>
