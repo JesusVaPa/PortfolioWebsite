4acd7bf12dbff28dc97049823f1c9f29608a963b
JesusVaPa <vapa.jesusangel@gmail.com>
2024-09-16 09:21:35 -0400

    github button implemented on portfolio section

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../css/portfolio.css';
+import githubBtn from "../media/githubbtn.mp4";
 
 function Portfolio() {
   return (
@@ -11,6 +12,14 @@ function Portfolio() {
       <p>
         (ORGANIZING MY PROJECTS ON GITHUB AND ON THE WAY TO DEPLOY.)
       </p>
+      <div className="video-container">
+      <a href="https://github.com/JesusVaPa" target="_blank" rel="noopener noreferrer">
+        <video className="clickable-video" width="100%" height="100%" autoPlay muted loop>
+          <source src={githubBtn} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </a>
+    </div>
     </div>
   );
 }
